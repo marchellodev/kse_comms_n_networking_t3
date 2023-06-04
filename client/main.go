d0a1feda4c92a5f4b6571d18cec63e7953e9f960
marchellodev <marchellodev@gmail.com>
2023-06-04 20:37:44 +0300

    sending & storing matrices

--- client/main.go
+++ client/main.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"encoding/binary"
 	"fmt"
+	"math/rand"
 	"net"
 	"os"
+	"time"
 )
 
 func main() {
@@ -32,12 +34,42 @@ func executeCLI(writer *bufio.Writer) {
 		command := scanner.Text()
 		switch command {
 		case "ping":
-			write(writer, 5)
+			write(writer, []uint32{5})
 			fmt.Println("> ping sent")
+
+		case "send_4":
+			matrix1 := generateMatrix(4)
+			matrix2 := generateMatrix(4)
+			fmt.Println("> matrices generated")
+
+			write(writer, []uint32{7, 4})
+			for _, row := range matrix1 {
+				write(writer, row)
+			}
+			for _, row := range matrix2 {
+				write(writer, row)
+			}
+
+			fmt.Println("> matrices sent")
+
 		default:
 			fmt.Println("Invalid command.")
 		}
 	}
+}
+
+func generateMatrix(size int) [][]uint32 {
+	matrix := make([][]uint32, size)
+	rand.Seed(time.Now().UnixNano())
+
+	for i := range matrix {
+		matrix[i] = make([]uint32, size)
+		for j := range matrix[i] {
+			matrix[i][j] = uint32(rand.Intn(1000)) // Generate a random value from 1 to 1000
+		}
+	}
+
+	return matrix
 }
 
 func initReader(reader *bufio.Reader) {
@@ -53,6 +85,12 @@ func initReader(reader *bufio.Reader) {
 			switch number {
 			case 6:
 				fmt.Println("> pong received")
+
+			case 8:
+				id := read(reader)
+
+				fmt.Println("> matrix pair stored with id:", id)
+
 			default:
 				fmt.Println("> unknown received:", number)
 			}
@@ -60,13 +98,32 @@ func initReader(reader *bufio.Reader) {
 	}()
 }
 
-func write(writer *bufio.Writer, number uint32) {
-	numBytes := make([]byte, 4)
-	binary.BigEndian.PutUint32(numBytes, number)
-	_, err := writer.Write([]byte(numBytes))
+func read(reader *bufio.Reader) uint32 {
+	buffer := make([]byte, 4)
+	_, err := reader.Read(buffer)
+	number := binary.BigEndian.Uint32(buffer)
 	if err != nil {
-		println("Write failed:", err.Error())
+		fmt.Println("Failed to read server response:", err.Error())
 		os.Exit(1)
 	}
-	err = writer.Flush()
+	return number;
+}
+
+// expects a row of numbers, automatically transforms them to bytes
+func write(writer *bufio.Writer, numbers []uint32) {
+	numBytes := make([]byte, 4)
+	for _, number := range numbers {
+		// fmt.Println("> writing:", number)
+		binary.BigEndian.PutUint32(numBytes, number)
+		_, err := writer.Write([]byte(numBytes))
+		if err != nil {
+			println("Write failed:", err.Error())
+			os.Exit(1)
+		}
+	}
+	err := writer.Flush()
+	if err != nil {
+		println("Flush failed:", err.Error())
+		os.Exit(1)
+	}
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,20 +2,25 @@ use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use std::{
     io::{prelude::*, BufReader, BufWriter, Cursor},
     net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex},
 };
+
+type Matrix = Vec<Vec<u32>>;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let matrices: Arc<Mutex<Vec<(Matrix, Matrix)>>> = Arc::new(Mutex::new(Vec::new()));
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
-        handle_connection(stream);
+        handle_connection(stream, &matrices.clone());
     }
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(stream: TcpStream, matrix_store: &Mutex<Vec<(Matrix, Matrix)>>) {
     let stream_clone = stream.try_clone().expect("Failed to clone TcpStream.");
 
     let mut buf_reader = BufReader::new(stream);
@@ -38,11 +43,65 @@ fn handle_connection(stream: TcpStream) {
                 buf_writer.write_u32::<BigEndian>(6).unwrap();
                 buf_writer.flush().unwrap();
             }
-            3 => println!("Three"),
-            4 => println!("Four"),
+            7 => {
+                println!(" > Preparing to receive a matrix");
+                let size = read_number(&mut buf_reader);
+                println!(" > Matrix size: {}x{}", size, size);
+
+                let mut matrix1 = Matrix::new();
+                let mut matrix2 = Matrix::new();
+                matrix1.resize(size as usize, vec![0; size as usize]);
+                matrix2.resize(size as usize, vec![0; size as usize]);
+
+                for i in 0..size {
+                    for j in 0..size {
+                        matrix1[i as usize][j as usize] = read_number(&mut buf_reader);
+                    }
+                }
+
+                for i in 0..size {
+                    for j in 0..size {
+                        matrix2[i as usize][j as usize] = read_number(&mut buf_reader);
+                    }
+                }
+
+                println!(" > Matrices received:");
+                print_matrix(&matrix1);
+                println!("");
+                print_matrix(&matrix2);
+                let mut lock = matrix_store.lock().unwrap();
+
+                let matrix_id = lock.len();
+
+                lock.push((matrix1, matrix2));
+                println!(" > Matrice pair stored with id: {}", matrix_id);
+                buf_writer.write_u32::<BigEndian>(8).unwrap();
+                buf_writer.write_u32::<BigEndian>(matrix_id as u32).unwrap();
+                buf_writer.flush().unwrap();
+            }
             _ => {
                 println!("Unknown request")
             }
         }
     }
+}
+
+fn read_number(reader: &mut BufReader<TcpStream>) -> u32 {
+    let mut buffer = [0; 4];
+    if reader.read_exact(&mut buffer).is_err() {
+        println!("Connection closed");
+    }
+
+    let mut rdr = Cursor::new(buffer);
+    rdr.read_u32::<BigEndian>().unwrap()
+}
+
+fn print_matrix(arr: &Matrix) {
+    arr.iter().for_each(|row| {
+        print!("[ ");
+        row.iter().for_each(|elem| {
+            print!("{:5} ", elem);
+        });
+        println!("]");
+    });
 }
